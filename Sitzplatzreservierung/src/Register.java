2f6deed15827555672e459b6fa61618d69036e25
Richard <Richard@192.168.1.19>
2013-05-13 18:18:37 +0200

    ActionListener eingebaut/Übergabe der textfeld eingaben an user klasse

--- Sitzplatzreservierung/src/Register.java
+++ Sitzplatzreservierung/src/Register.java
@@ -1,18 +1,28 @@
 import java.awt.BorderLayout;
 import java.awt.FlowLayout;
 import java.awt.GridLayout;
-
+import java.awt.*;
+import java.awt.event.*;
 import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
 import javax.swing.JTextField;
-
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 
 public class Register extends JFrame {
 	
 	//Organisation
+	
+	// Variablen für Klasse User
+	String name;
+	String firstName;
+	String email;
+	
+	///////////////////////
+	
 	private JLabel instructionsLabel = new JLabel();
 	private JLabel nameLabel = new JLabel();
 	private JLabel firstNameLabel = new JLabel();
@@ -29,7 +39,11 @@ public class Register extends JFrame {
 	public JTextField birthdayTF = new JTextField(25);
 	public JTextField emailTF = new JTextField(25);
 	
-	public JButton registerButton = new JButton("Registrieren");
+	//public JButton registerButton = new JButton("Registrieren");
+	
+	//registerButton.addActionListener (new FooActionListener (nameTF));
+	
+	//////////////////////////////////
 	
 	
 	
@@ -38,6 +52,8 @@ public class Register extends JFrame {
 		setDefaultCloseOperation(EXIT_ON_CLOSE);
 		setSize(600,600);
 		setLayout(new BorderLayout());
+		
+		
 		
 		//Header-Panel
 		northPanel = new JPanel(new GridLayout(10,0));
@@ -68,11 +84,71 @@ public class Register extends JFrame {
 		eastPanel.add(birthdayTF);
 		eastPanel.add(emailTF);
 		
+		//Actionlistener für Registerbutton
+		JButton registerButton = new JButton("Registrieren");
+		registerButton.addActionListener (new NameActionListener (nameTF));
+		registerButton.addActionListener (new FirstNameActionListener (firstNameTF));
+		registerButton.addActionListener (new EmailActionListener (emailTF));
 		southPanel.add(registerButton);
 	}
+	
+	//ActionListener für Nachnamen
+	public class NameActionListener implements ActionListener {
+	      
+	    private JTextField nameTF;
+	      
+	    public NameActionListener (JTextField nameTF){
+	        this.nameTF = nameTF;
+	    }
+	      
+	    @Override
+	    public void actionPerformed (ActionEvent e){
+	    	name = nameTF.getText();
+	       System.out.println (name);
+	    }
+    }
+	
+		
+	//ActionListener für Vornamen
+	public class FirstNameActionListener implements ActionListener {
+		      
+		 private JTextField firstNameTF;
+		      
+		 public FirstNameActionListener (JTextField firstNameTF){
+	        this.firstNameTF = firstNameTF;
+	    }
+		      
+	  @Override
+		 public void actionPerformed (ActionEvent e){
+		  firstName = firstNameTF.getText();
+	       System.out.println (firstName);
+		 }
+    }
+	
+	//ActionListener für E-Mail
+	public class EmailActionListener implements ActionListener {
+				      
+	    private JTextField emailTF;
+				      
+	    public EmailActionListener (JTextField emailTF){
+			 this.emailTF = emailTF;
+	   }
+				      
+	   @Override
+		 public void actionPerformed (ActionEvent e){
+		   email = emailTF.getText();
+	       System.out.println (email);
+	    }
+	}
+	
+
+		//////////////////////////////////
+	
+	
 	
 	public static void main (String[] args){
 		Register app = new Register();
 		app.setVisible(true);
+		User user = new User(name, firstName, email);
 	}
 }
