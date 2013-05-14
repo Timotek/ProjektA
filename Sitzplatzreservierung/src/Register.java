5e74c807a943daff724d4eee74992ec2edd6f8cf
Richard <Richard@192.168.1.19>
2013-05-14 21:36:41 +0200

    IDs entfernt, Register um Passwort und Username erweitert, Speicherung der User Daten mit username.txt

--- Sitzplatzreservierung/src/Register.java
+++ Sitzplatzreservierung/src/Register.java
@@ -12,161 +12,207 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
 
-public class Register extends JFrame {
-	
-	//Organisation
-	
-	// Variablen für Klasse User
-	String name;
-	String firstName;
-	String email;
-	int ID = 1;
-	
-	///////////////////////
-	
-	private JLabel instructionsLabel = new JLabel();
-	private JLabel nameLabel = new JLabel();
-	private JLabel firstNameLabel = new JLabel();
-	private JLabel birthdayLabel = new JLabel();
-	private JLabel emailLabel = new JLabel();
-	
-	public JPanel northPanel;
-	public JPanel westPanel;
-	public JPanel eastPanel;
-	public JPanel southPanel;
-	
-	public JTextField nameTF = new JTextField(25);
-	public JTextField firstNameTF = new JTextField(25);
-	public JTextField birthdayTF = new JTextField(25);
-	public JTextField emailTF = new JTextField(25);
-	
-	
-	//////////////////////////////////
-	
-	
-	
-	public Register(){
-		super("Registrierung");
-		setDefaultCloseOperation(EXIT_ON_CLOSE);
-		setSize(600,600);
-		setLayout(new BorderLayout());
-		
-		
-		
-		//Header-Panel
-		northPanel = new JPanel(new GridLayout(10,0));
-		add(northPanel, BorderLayout.NORTH);
-		
-		instructionsLabel.setText("Bitte füllen Sie folgendes Formular vollständig aus: ");
-		northPanel.add(instructionsLabel);
-		
-		//Formular-Panel
-		westPanel = new JPanel(new GridLayout(12,0));
-		add(westPanel, BorderLayout.WEST);
-		eastPanel = new JPanel(new GridLayout(12,0));
-		add(eastPanel, BorderLayout.EAST);
-		southPanel = new JPanel(new FlowLayout());
-		add(southPanel, BorderLayout.SOUTH);
-		
-		nameLabel.setText("Name: ");
-		firstNameLabel.setText("Vorname: ");
-		birthdayLabel.setText("Geburtsdatum: (TT.MM.JJJJ");
-		emailLabel.setText("Email-Adresse: (mustermann@domain.de");
-		westPanel.add(nameLabel);
-		westPanel.add(firstNameLabel);
-		westPanel.add(birthdayLabel);
-		westPanel.add(emailLabel);
-		
-		eastPanel.add(nameTF);
-		eastPanel.add(firstNameTF);		
-		eastPanel.add(birthdayTF);
-		eastPanel.add(emailTF);
-		
-		//Actionlistener für Registerbutton
-		JButton registerButton = new JButton("Registrieren");
-		registerButton.addActionListener (new NameActionListener (nameTF));
-		registerButton.addActionListener (new FirstNameActionListener (firstNameTF));
-		registerButton.addActionListener (new EmailActionListener (emailTF));
-		southPanel.add(registerButton);
-	
-	}
-	
-	//ActionListener für Nachnamen
-	public class NameActionListener implements ActionListener {
-	      
-	    private JTextField nameTF;
-	      
-	    public NameActionListener (JTextField nameTF){
-	        this.nameTF = nameTF;
-	    }
-	      
-	    @Override
-	    public void actionPerformed (ActionEvent e){
-	    	name = nameTF.getText();
-	       System.out.println (name);
-	    sendData();
-	    }
-    }
-	
-		
-	//ActionListener für Vornamen
-	public class FirstNameActionListener implements ActionListener {
-		      
-		 private JTextField firstNameTF;
-		      
-		 public FirstNameActionListener (JTextField firstNameTF){
-	        this.firstNameTF = firstNameTF;
-	    }
-		      
-	  @Override
-		 public void actionPerformed (ActionEvent e){
-		  firstName = firstNameTF.getText();
-	       System.out.println (firstName);
-	      //sendData();
-		 }
-    }
-	
-	//ActionListener für E-Mail
-	public class EmailActionListener implements ActionListener {
-				      
-	    private JTextField emailTF;
-				      
-	    public EmailActionListener (JTextField emailTF){
-			 this.emailTF = emailTF;
-	   }
-				      
-	   @Override
-		 public void actionPerformed (ActionEvent e){
-		   email = emailTF.getText();
-	       System.out.println (email);
-	       //sendData();
-	    }
-	}
-	
-	/*
-	public void createID(){
-		
-		ID = ID + 1;
-		System.out.println("ID wurde erzeugt");
-		sendData();
-	}
-	*/
-	
-	//ID wird hier hochgezählt
-	public void sendData(){
-		
-		System.out.println("ID wurde erzeugt");
-		new User(ID, name, firstName, email);
-		System.out.println("SendData wurde ausgeführt");
-		ID++;
-	}
-		//////////////////////////////////
-	
-	
-	
-	public static void main (String[] args){
-		Register app = new Register();
-		app.setVisible(true);
+	public class Register extends JFrame {
 
+		//Organisation
+
+		// Variablen für Klasse User
+		String name;
+		String firstName;
+		String email;
+		String userName;       						  //neu
+		String pw;              					  //neu
+		//int ID = 1;
+
+		///////////////////////
+
+		private JLabel instructionsLabel = new JLabel();
+		private JLabel firstNameLabel = new JLabel();
+		private JLabel nameLabel = new JLabel();
+		private JLabel birthdayLabel = new JLabel();
+		private JLabel emailLabel = new JLabel();
+		private JLabel userNameLabel = new JLabel();    	   //neu
+		private JLabel pwLabel = new JLabel();          	   //neu
+		
+		public JPanel northPanel;
+		public JPanel westPanel;
+		public JPanel eastPanel;
+		public JPanel southPanel;
+
+		public JTextField firstNameTF = new JTextField(25);
+		public JTextField nameTF = new JTextField(25);
+		public JTextField birthdayTF = new JTextField(25);
+		public JTextField emailTF = new JTextField(25);
+		public JTextField userNameTF = new JTextField(25);				//neu
+		public JTextField pwTF = new JTextField(25);					//neu
+
+		//////////////////////////////////
+
+
+
+		public Register(){
+			super("Registrierung");
+			setDefaultCloseOperation(EXIT_ON_CLOSE);
+			setSize(600,600);
+			setLayout(new BorderLayout());
+
+
+
+			//Header-Panel
+			northPanel = new JPanel(new GridLayout(10,0));
+			add(northPanel, BorderLayout.NORTH);
+
+			instructionsLabel.setText("Bitte füllen Sie folgendes Formular vollständig aus: ");
+			northPanel.add(instructionsLabel);
+
+			//Formular-Panel
+			westPanel = new JPanel(new GridLayout(12,0));
+			add(westPanel, BorderLayout.WEST);
+			eastPanel = new JPanel(new GridLayout(12,0));
+			add(eastPanel, BorderLayout.EAST);
+			southPanel = new JPanel(new FlowLayout());
+			add(southPanel, BorderLayout.SOUTH);
+
+			firstNameLabel.setText(" Vorname: ");
+			nameLabel.setText(" Name: ");
+			userNameLabel.setText(" Username:");									//neu
+			pwLabel.setText(" Passwort");										//neu
+			birthdayLabel.setText(" Geburtsdatum:   (TT.MM.JJJJ)");
+			emailLabel.setText(" Email-Adresse:   (mustermann@domain.de)");
+			
+			westPanel.add(firstNameLabel);
+			westPanel.add(nameLabel);
+			westPanel.add(userNameLabel);						//neu
+			westPanel.add(pwLabel);								//neu
+			westPanel.add(birthdayLabel);
+			westPanel.add(emailLabel);
+
+			eastPanel.add(firstNameTF);	
+			eastPanel.add(nameTF);
+			eastPanel.add(userNameTF);							//neu
+			eastPanel.add(pwTF);								//neu
+			eastPanel.add(birthdayTF);
+			eastPanel.add(emailTF);
+
+			//Actionlistener für Registerbutton
+			JButton registerButton = new JButton("Registrieren");
+			registerButton.addActionListener (new NameActionListener (nameTF));
+			registerButton.addActionListener (new FirstNameActionListener (firstNameTF));
+			registerButton.addActionListener (new EmailActionListener (emailTF));
+			registerButton.addActionListener (new UserNameActionListener (userNameTF));
+			registerButton.addActionListener (new pwActionListener (pwTF));
+			southPanel.add(registerButton);
+
+		}
+
+		////////////////////////////////////////////////////////////////////////
+		
+		//ActionListener für Nachnamen
+		public class NameActionListener implements ActionListener {
+
+			private JTextField nameTF;
+
+			public NameActionListener (JTextField nameTF){
+				this.nameTF = nameTF;
+			}
+
+			@Override
+			public void actionPerformed (ActionEvent e){
+				name = nameTF.getText();
+				System.out.println (name);
+				sendData();
+			}
+		}
+
+
+		//ActionListener für Vornamen
+		public class FirstNameActionListener implements ActionListener {
+
+			private JTextField firstNameTF;
+
+			public FirstNameActionListener (JTextField firstNameTF){
+				this.firstNameTF = firstNameTF;
+			}
+
+			@Override
+			public void actionPerformed (ActionEvent e){
+				firstName = firstNameTF.getText();
+				System.out.println (firstName);
+				
+			}
+		}
+		
+		
+		//ActionListener für Username
+				public class UserNameActionListener implements ActionListener {
+
+					private JTextField userNameTF;
+
+					public UserNameActionListener (JTextField userNameTF){
+						this.userNameTF = userNameTF;
+					}
+
+					@Override
+					public void actionPerformed (ActionEvent e){
+						userName = userNameTF.getText();
+						System.out.println (userName);
+						
+					}
+				}
+				
+				
+				//ActionListener für pw
+				public class pwActionListener implements ActionListener {
+
+					private JTextField pwTF;
+
+					public pwActionListener (JTextField pwTF){
+						this.pwTF = pwTF;
+					}
+
+					@Override
+					public void actionPerformed (ActionEvent e){
+						pw = pwTF.getText();
+						System.out.println (pw);
+						
+					}
+				}		
+		
+
+		//ActionListener für E-Mail
+		public class EmailActionListener implements ActionListener {
+
+			private JTextField emailTF;
+
+			public EmailActionListener (JTextField emailTF){
+				this.emailTF = emailTF;
+			}
+
+			@Override
+			public void actionPerformed (ActionEvent e){
+				email = emailTF.getText();
+				System.out.println (email);
+				
+			}
+		}
+
+		
+
+		//ID wird hier hochgezählt
+		public void sendData(){
+
+		//	System.out.println("ID wurde erzeugt");
+			new User(name, firstName, email, userName, pw);
+			System.out.println("SendData wurde ausgeführt");
+			//ID++;
+		}
+		//////////////////////////////////
+
+
+
+		public static void main (String[] args){
+			Register app = new Register();
+			app.setVisible(true);
+
+		}
 	}
-}
--- Sitzplatzreservierung/src/User.java
+++ Sitzplatzreservierung/src/User.java
@@ -4,40 +4,47 @@ import java.io.PrintWriter;
 
 public class User {
 	
-	private int ID;
+  //private int ID;
 	private String name;
 	private String firstName;
 	private String email;
+	private String userName;							//neu
+	private String pw;									//neu
 	private String filename;
 	FileManager fm = new FileManager();
 
 	//IDim Konstruktor hinzugefügt
-	public User(int ID, String name, String firstName, String email){
-		this.ID = ID;
+	public User(String name, String firstName, String email, String userName, String pw){
+	  //this.ID = ID;
 		this.name = name;
 		this.firstName = firstName;
 		this.email = email;
+		this.userName = userName;
+		this.pw = pw;
 		createUser();
 
 	}
 	public void createUser(){				// ID auskommentiert
 	//	ID = ID+1;
-		filename = "USER" + ID;
-		fm.createFile(filename+".txt");
-		System.out.println("Datei "+filename+".txt erstellt");
+	//	filename = "USER" + ID;
+		filename = userName;
+		fm.createFile(filename + ".txt");
+		System.out.println("Datei " + filename + ".txt erstellt");
 		
-		sendUserData(ID ,name, firstName, email);
+		sendUserData(name, firstName, email, userName, pw);
 	//	ID++;
 	}
 	
 
-	public void sendUserData(Object ID, Object object1, Object object2, Object object3){
+	public void sendUserData(Object object1, Object object2, Object object3, Object object4, Object object5){
 
 		try (final PrintWriter userWriter = new PrintWriter(FileManager.file);) {
-			userWriter.println(ID);
+		//	userWriter.println(ID);
 			userWriter.println(object1);
 			userWriter.println(object2);
 			userWriter.println(object3);
+			userWriter.println(object4);
+			userWriter.println(object5);
 			userWriter.close();
 		}
 		catch (FileNotFoundException e) {
