e58faffb4452345587c26e44865b6135bffdaaa4
Richard <Richard@192.168.1.13>
2013-05-30 21:22:10 +0200

    Loginfunktion: Suche nach user.txt, Vergleich des Passworts etc

--- Sitzplatzreservierung/src/Login.java
+++ Sitzplatzreservierung/src/Login.java
@@ -1,5 +1,9 @@
 import java.awt.GridLayout;
-
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.util.ArrayList;
+import java.io.*;
+import java.util.*;
 import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
@@ -7,7 +11,7 @@ import javax.swing.JTextField;
 
 public class Login extends JFrame{
 	
-	//Inititalisierung der Swin-Komponenten
+	//Inititalisierung der Swing-Komponenten
 	JLabel userLabel = new JLabel("Username");
 	JLabel passLabel = new JLabel("Password");
 	
@@ -15,7 +19,12 @@ public class Login extends JFrame{
 	JTextField passField = new JTextField();
 	
 	JButton loginButton = new JButton("Login");
-	JButton registerButton = new JButton("New Here? Sing up!");
+	JButton registerButton = new JButton("New Here? Sign up!");
+	
+	String userName;
+	String pw;
+	
+	FileManager fileM = new FileManager(); // Filemanager neues Objekt
 	
 	public Login(){
 		
@@ -29,10 +38,78 @@ public class Login extends JFrame{
 		add(userField);
 		add(passLabel);
 		add(passField);
+		loginButton.addActionListener (new UserNameActionListener (userField, passField));
 		add(loginButton);
 		add(registerButton);
-		
 	}
+	
+	
+	//Login Erweiterung, Vergleich des Passworts in der user.txt 
+	//actionlistener für login button
+	
+	public class UserNameActionListener implements ActionListener {
+
+		private JTextField userField;
+		private JTextField passField;
+
+		public UserNameActionListener (JTextField userField, JTextField passField){
+			this.userField = userField;
+			this.passField = passField;
+			
+		}
+		@Override
+		public void actionPerformed (ActionEvent e){
+			userName = userField.getText();
+			pw = passField.getText();
+			System.out.println (userName);
+			System.out.println(pw);
+			saveUserData();
+			compareUserData();		
+		}
+	}
+	
+		ArrayList userList = new ArrayList<Double>();
+		//Scannen der txt Datei, Schreiben der Zeilen in ArrayList
+		
+		public void saveUserData(){	
+			try {
+				Scanner scan = new Scanner(new File(userName + ".txt"));
+				while(scan.hasNextLine()) {
+					userList.add(scan.nextLine());
+				}
+				scan.close();			
+				Iterator it = userList.iterator();
+				while(it.hasNext()) {
+					System.out.println(it.next());
+				}
+			} 
+			catch(FileNotFoundException e) {
+				System.out.println("Fehler: Quelldatei existiert nicht");
+				System.exit(1);
+			} 
+			catch(Exception e) {
+				System.out.println("Fehler: Ein unbekannter Fehler ist aufgetreten");
+				System.exit(1);
+			}
+		}
+			
+		//nächster Schritt: arraylist durchsuchen nach eingegebenem Passwort
+	
+		public void compareUserData(){
+
+			if(pw.equals(userList.get(4)) ){
+				System.out.println("Passwörter sind gleich!");
+				System.out.println("Eingegebenes Passwort: " + pw);
+				System.out.println("Gespeichertes Passwort: " + userList.get(4));
+			}
+			else {
+				System.out.println("Passwörter nicht gleich!");
+			}
+				// eigentliche Funktion: Weiterleitung zur entsprechenden Seite bei erfolgreichem Login
+				// bzw. bei fehlgeschlagenem Login JDialog Meldung
+	    }
+
+	
 	
 	public static void main(String[]args){
 		Login app = new Login();
